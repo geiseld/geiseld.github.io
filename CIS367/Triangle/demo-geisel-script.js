6cc34825b6ff8847ecbe8f6359a776eaaf3f3efb
David Geisel <geiseld@mail.gvsu.edu>
2024-01-27 14:36:45 -0500

    adding 367 stuff

---
+++ CIS367/Triangle/demo-geisel-script.js
@@ -0,0 +1,54 @@
+/*
+demo-geisel-script.js
+David Geisel
+*/
+
+// Global variables we'll need
+var gl;
+var points;
+var variables;
+
+// This function executes our WebGL code AFTER the window is loaded.
+// Meaning, that we wait for our canvas element to exist.
+window.onload = function init() {
+  // Grab the canvas object and initialize it
+  var canvas = document.getElementById('gl-canvas');
+  gl = WebGLUtils.setupWebGL(canvas);
+
+  // Error checking
+  if (!gl) { alert('WebGL unavailable'); }
+
+  // triangle vertices
+  vertices = [
+    vec2(-0.5, -0.5),
+    vec2(0, 0.5),
+    vec2(0.5, -0.5),
+
+  ];
+
+  // configure WebGL
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(0.9,0.9,0.9,1.0);
+
+  // load shaders and initialize attribute buffers
+  var program = initShaders(gl, 'vertex-shader', 'fragment-shader');
+  gl.useProgram(program);
+
+  // load data into GPU
+  var bufferID = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, bufferID);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+  // set its position and render it
+  var vPosition = gl.getAttribLocation(program, 'vPosition');
+  gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
+  render();
+};
+
+// Render whatever is in our gl variable
+function render() {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length);
+}
+
